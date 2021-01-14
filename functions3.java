d3b48e11f2fc536c812e7d9b138e6ee95a49dd2d
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-14 20:40:10 +0200

    Update functions3.java

--- functions3.java
+++ functions3.java
@@ -5,18 +5,28 @@ import java.util.Scanner;
 
 public class Main
 {
+    static Scanner s= new Scanner(System.in); // scanner here in parent
+    static Random r = new Random();
 
+    private static float getFloat(String msg) {
+        System.out.println(msg);
+        float f = s.nextFloat(); // scanner here in child automatically
+        return f;
+    }
+    private static int random1To100()    {
+        int rnd = r.nextInt(100) + 1;
+        return rnd;
+    }
 
     public static void main(String[] args)  {
-        Scanner s = new Scanner(System.in);
-        Random r = new Random();
 
+        getFloat("Enter float:");
         // write a function that gets as parameter Scanner s and String msg
         // println(msg)
         // s.nextFloat
         // return the float
 
-        // write a function that gets as parameter Random R 
+        // write a function that gets as parameter Random R
         // and returns a random number between 1-100 (0-99) + 1
 
         // create a function that prints an int[] array
