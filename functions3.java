9df13bb6b6358ca555781adb6d78959945ff9810
qa191120 <74743291+qa191120@users.noreply.github.com>
2021-01-14 20:12:26 +0200

    Update functions3.java

--- functions3.java
+++ functions3.java
@@ -6,23 +6,27 @@ import java.util.Scanner;
 public class Main
 {
 
-    
+
     public static void main(String[] args)  {
         Scanner s = new Scanner(System.in);
         Random r = new Random();
 
-        // write a function that gets as parameter Scanner and String
-        // println the string and s.nextFloat
+        // write a function that gets as parameter Scanner s and String msg
+        // println(msg)
+        // s.nextFloat
         // return the float
+
+        // write a function that gets as parameter Random R 
+        // and returns a random number between 1-100 (0-99) + 1
 
         // create a function that prints an int[] array
         // write a function that gets an array int[] and puts zeros in the array
         // in main function create array with {1,2,3}
         // send the array to the print function
-        // send the array to the zero fucntion you just written
+        // send the array to the zero function you just written
         // send the array to the print function
 
-        
+
 
     }
 
